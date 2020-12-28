6c9bce059bd3499acaeda6bb3043381a962204fe
Muhamad Virgiawan Dwi Rangga <virgiawndwir@gmail.com>
2020-12-28 15:24:10 +0700

    add admin lte

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Profile')
+
+@section('content_header')
+<h1>Profile</h1>
+@stop
+
+@section('content')
+<p>Hola {{ $user }} !</p>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
